05110a52ca0f9d54ef661f11c5ef792a85cfa297
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-03-21 15:17:44 +0530

    nothing

--- helper/ipaddress.js
+++ helper/ipaddress.js
@@ -3,7 +3,7 @@ module.exports={
         function ipaddressreturn() {
             // return "http://192.168.43.163:3001"
 
-            return "http://54.196.129.41:3001/"
+            return "http://54.196.129.41:3001"
         },
     ipvalreturnwithoutport:
         function ipaddressreturn() {
