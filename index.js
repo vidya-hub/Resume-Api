966655e27664b2821fcca41600d32f2d9d875479
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-06-16 19:46:27 +0530

    Test The CSS styles

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ mongoose.connection.once('open', function () {
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.static(path.join(__dirname, 'data')));
     app.use("/data", express.static(__dirname + "/data"));
-
     app.use("/public", express.static(__dirname + "/public/"));
+    // app.use("/public", express.static(__dirname + "/public/"));
     app.use(express.static('ui'));
     app.post('/*', route);
     app.get('/*', route);
